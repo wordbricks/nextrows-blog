150a290b04be7749dcc157c8a4e4d97400d5fbe2
lentil32 <lentil32@icloud.com>
2025-09-11 21:26:01 +0900

    feat: Add code highlight

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,11 +1,17 @@
 import { remark } from "remark";
-import html from "remark-html";
 import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
+import rehypeHighlight from "rehype-highlight";
+import rehypeStringify from "rehype-stringify";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark()
+  const file = await remark()
     .use(remarkGfm)
-    .use(html, { sanitize: false })
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeHighlight, { detect: true })
+    .use(rehypeStringify)
     .process(markdown);
-  return result.toString();
+  return String(file);
 }
