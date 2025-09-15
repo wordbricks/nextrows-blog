738405fb145f05f993a2818a95a990357002e8ed
lentil32 <lentil32@icloud.com>
2025-09-15 11:04:08 +0900

    chore: Update host

--- src/env/host.ts
+++ src/env/host.ts
@@ -4,6 +4,6 @@ import { isServer } from "@/env/isServer";
 const localHost = isServer() ? "localhost" : location.hostname;
 
 export const HOST = {
-  [ENV.PROD]: `https://blog.nextrows.com`,
+  [ENV.PROD]: `https://nextrows-blog.vercel.app`,
   [ENV.LOCAL]: `http://${localHost}:4321`,
 } as const;
