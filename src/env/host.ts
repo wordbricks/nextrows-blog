e994e766963a1790954b18a4bfe5b6c90dee784b
lentil32 <lentil32@icloud.com>
2025-09-15 14:10:58 +0900

    feat: Change HOST

--- src/env/host.ts
+++ src/env/host.ts
@@ -4,6 +4,6 @@ import { isServer } from "@/env/isServer";
 const localHost = isServer() ? "localhost" : location.hostname;
 
 export const HOST = {
-  [ENV.PROD]: `https://nextrows-blog.vercel.app`,
+  [ENV.PROD]: `https://nextrows.com`,
   [ENV.LOCAL]: `http://${localHost}:4321`,
 } as const;
